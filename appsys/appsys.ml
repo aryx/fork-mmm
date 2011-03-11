515875e41b5bd85ca329856eadd4e8e8e4db4175
pad <pad@fb.com>
2011-03-11 11:13:54 -0800

    appsys.ml: removed use of dynlink for now. Need path to ocaml lib.

--- appsys/appsys.ml
+++ appsys/appsys.ml
@@ -76,7 +76,10 @@ let init initialp =
   Dynlink.init();
   Dynlink.add_available_units Crcs.crc_unit_list;
   Dynlink.add_available_units Crcsmmm.crc_unit_list;
-  Dynlink.add_interfaces ["Pervasives"; "Unix"] ["/opt/local/lib/ocaml"];
+(* pad: TODO, infer using `ocamlc -where`
+  Dynlink.add_interfaces ["Pervasives"; "Unix"] 
+    ["/opt/local/lib/ocaml"];
+*)
   (* Load local applets *)
   if initialp then load_initial_modules();
   Mmmprefs.plug_applets applets_pref
