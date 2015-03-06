de99791b602c6a1ebfe2c7bbcfc7bbc626db795b
pad <pad@fb.com>
2015-03-05 16:06:06 -0800

    * viewers/viewers.ml: revert the modif I made because of camlp4o bug with ocamldoc

--- viewers/viewers.ml
+++ viewers/viewers.ml
@@ -49,6 +49,7 @@ type hyper_func = {
   }
 (*e: type Viewers.hyper_func *)
 
+(*s: class Viewers.context *)
 class virtual context ((did : Document.document_id), 
                (v : vparams)) =
  object (self : 'a)
@@ -90,6 +91,7 @@ class virtual context ((did : Document.document_id),
 
   method virtual log : string -> unit
 end
+(*e: class Viewers.context *)
 
 
 
